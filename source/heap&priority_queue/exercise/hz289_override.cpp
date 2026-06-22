4b9ff2a895862833ea468291b1222e16ef6b4b10
shengqing Qiu <2197576614@qq.com>
2026-06-22 11:17:35 +0800

    更新生日礼物和插入排序学习记录

--- source/heap&priority_queue/exercise/hz289_override.cpp
+++ source/heap&priority_queue/exercise/hz289_override.cpp
@@ -7,15 +7,16 @@ using namespace std;
 typedef pair<int,int> PII;
 
 #define MAX_N 100000
-long long arr[MAX_N + 5];
-int l[MAX_N+5];
-int r[MAX_N+5];
 
 void test01(){
     int n = 0;
     int m = 0;
     cin>>n;
     cin>>m;
+    long long arr[MAX_N + 5];
+    int l[MAX_N+5];
+    int r[MAX_N+5];
+
     int k = 1;
     cin>>arr[1];
     for(int i = 1;i<n;i++){
@@ -34,6 +35,7 @@ void test01(){
     int cnt = 0;//正数的个数cnt
     long long sum = 0;//正数的和值sum
     set<PII> s;
+
     for(int i = 1;i<=n;i++){
         if(arr[i] > 0){
             cnt++;
@@ -71,6 +73,70 @@ void test01(){
     }
     cout<<sum<<endl;
 }
+
+void test02(){
+    int n = 0;
+    int m = 0;
+    cin>>n;
+    cin>>m;
+    vector<int> arr(MAX_N+5);
+    cin>>arr[1];
+    int k = 1;
+    for(int i=1;i<n;i++){
+        int number;
+        cin>>number;
+        if(arr[k]*number<0){
+            k++;
+            arr[k]=number;
+        }else{
+            arr[k] += number;
+        }
+    }
+    n=k;
+    set<PII> s;
+    int cnt = 0;
+    int sum = 0;
+    vector<int> l(MAX_N+5);
+    vector<int> r(MAX_N+5);
+    for(int i =1 ;i<=n;i++){
+        if(arr[i] > 0){
+            cnt++;
+            sum+=arr[i];
+        }
+        l[i] = i-1;
+        r[i] = i+1;
+        s.insert(PII(abs(arr[i]),i));
+    }
+    vector<int> st(MAX_N+5);
+    while(cnt>m){
+        while(st[s.begin()->second] == 1) s.erase(s.begin());
+        int pos = s.begin()->second;
+        int right = r[pos];
+        int left = l[pos];
+        int number = s.begin()->first;
+        s.erase(s.begin());
+        //合并操作
+        if((left>=1 and right<=n) or arr[pos]>0){
+            sum -=number;
+            cnt--;
+            if(left >=1){
+                arr[pos] += arr[left];
+                l[r[left]] = l[left];
+                r[l[left]] = r[left];
+                st[left] = 1;
+            }
+            if(right <=n){
+                arr[pos] += arr[right];
+                l[r[right]] = l[right];
+                r[l[right]] = r[right];
+                st[right] = 1;
+            }
+            s.insert(PII(abs(arr[pos]),pos));
+        }
+    }
+    cout<<sum<<endl;
+}
+
 
 int main(void){
     test01();
--- source/sort_algorithm/base_algorithm/insertion_sort.cpp
+++ source/sort_algorithm/base_algorithm/insertion_sort.cpp
@@ -57,9 +57,33 @@ void insert_sort(int* arr,int l,int r){
     return;
 }
 
+
+//无监督的 插入排序
+void unsupervised_insert_sort(int* arr,int l,int r){
+    //把全局最小值先放到第一个位置上来
+    //可以去掉j>l的判断条件
+    int index = l;
+    for(int i = l+1;i<r;i++){
+        if(arr[i] <arr[index]) index = i;
+    }
+    //swap(&arr[l],&arr[index]);
+    while(index > l){
+        swap(&arr[index],&arr[index-1]);
+        index--;
+    }
+    for(int i = l+1;i<r;i++){
+        int j = i;
+        while(arr[j]<arr[j-1]){
+            swap(&arr[j],&arr[j-1]);
+            j--;
+        }
+    }
+}
+
 void test01(){
     int* arr = get_rand_data(SMALL_DATA_N);
     TEST(insert_sort,arr,SMALL_DATA_N);
+    TEST(unsupervised_insert_sort,arr,SMALL_DATA_N);
     free(arr);
     return;
 }
@@ -67,6 +91,7 @@ void test01(){
 void test02(){
     int* arr = get_rand_data(BIG_DATA_N);
     TEST(insert_sort,arr,BIG_DATA_N);
+    TEST(unsupervised_insert_sort,arr,BIG_DATA_N);
     free(arr);
     return;
 }
